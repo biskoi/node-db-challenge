d28aa325ded41b4004f0366b9ce5e089dee7a4ee
biskoi <biskoi@daydream.cafe>
2020-04-17 09:35:12 -0600

    wrote migrations file

---
+++ data/migrations/20200417091458_create tables.js
@@ -0,0 +1,52 @@
+
+exports.up = function(knex) {
+  return knex.schema
+
+   .createTable('projects', prj => {
+      prj.increments('id').primary().unique();
+      prj.string('name', 255).notNullable().unique().index();
+      prj.string('description', 255).notNullable();
+      prj.integer('completed', 255).defaultTo(0).index();
+   })
+
+   .createTable('resources', rsrc => {
+      rsrc.increments('id', 255).primary().unique();
+      rsrc.string('name', 255).notNullable().unique().index();
+      rsrc.string('description', 255);
+   })
+
+   .createTable('tasks', tsk => {
+      tsk.increments('id', 255).primary().unique();
+      tsk.string('project_id', 255)
+         .notNullable()
+         .references('projects.id')
+         .onDelete('RESTRICT')
+         .onUpdate('CASCADE');
+      tsk.string('description', 255).notNullable();
+      tsk.string('notes', 255);
+      tsk.integer('completed', 255).defaultTo(0);
+   })
+
+   .createTable('project_resources', prsrc => {
+      prsrc.increments('id', 255).primary();
+      prsrc.string('project_id', 255)
+         .notNullable()
+         .references('projects.id')
+         .onDelete('RESTRICT')
+         .onUpdate('CASCADE');
+      prsrc.string('resource_id', 255)
+         .references('resources.id')
+         .notNullable()
+         .onDelete('RESTRICT')
+         .onUpdate('CASCADE')
+   })
+
+};
+
+exports.down = function(knex) {
+  return knex.schema
+  .dropTableIfExists('project_resources')
+  .dropTableIfExists('tasks')
+  .dropTableIfExists('resources')
+  .dropTableIfExists('projects')
+};
